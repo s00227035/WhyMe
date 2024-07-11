9a49a9918905c53d18df486e84627fd4f68bd1a8
s00227035 <s00227035@atu.ie>
2024-07-11 20:14:35 +0100

    022 - Enemy pathfinding
    Testing and changing the enemy script, trying to get the logic together with pathfinding package

--- WhyMe/Assets/Scripts/Enemyy.cs
+++ WhyMe/Assets/Scripts/Enemyy.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Enemyy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AIPath aiPath;
+    public int damage = 40;
+    public float AttackRange = 3f;
+    public float sprintMovementSpeed = 6.5f; // sprint speed
+    public float slowedMovementSpeed = 2f; // slowed speed
+    public float attackCooldown = 3f; // Cooldown between attacks
+    public float slowedCooldown = 7f; // Time to remain slowed after attack
+
+    private GameObject player;
+    private Animator animator;
+    private float attackTimer; // Timer for attack cooldown
+    private float slowedTimer; // Timer for slowed state
+
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         
