87a00474ae18375f55be90427be1986d06e7a0d2
s00227035 <s00227035@atu.ie>
2024-07-31 12:14:06 +0100

    048 - UI
    Added scene MainMenu, configured trigger after the door to pop the UI with button options

--- WhyMe/Assets/Scripts/ExitUIManager.cs
+++ WhyMe/Assets/Scripts/ExitUIManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject exitUIPanel; //Reference to the UI panel
+
+    private void Awake()
     {
-        
+        if (exitUIPanel != null)
+        {
+            exitUIPanel.SetActive(false); //Hide the panel on start
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //Method to handle Main menu button
+    public void OnMainMenuButtonClicked()
     {
-        
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    //handle restart button
+    public void OnRestartButtonClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //Reload the current scene
+    }
+
+    //handle quit button
+    public void OnQuitButtonClick()
+    {
+        Application.Quit(); //Quit the app
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; //Stop play mode in Unity Editor
+        #endif
     }
 }
