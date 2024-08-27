41947b696e80738a35c052cc18ff22afbfdb2e76
s00227035 <s00227035@atu.ie>
2024-08-27 20:51:47 +0100

    071 - Cleanup
    Final cleanup of the project

--- WhyMe/Assets/Scripts/DraggableBox.cs
+++ WhyMe/Assets/Scripts/DraggableBox.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Unity.Collections.AllocatorManager;
-using static UnityEditor.Progress;
 
 public class DraggableBox : MonoBehaviour
 {
